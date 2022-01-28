e139e43f6c2beb064f983c332c173e2c82554393
Mario De Luca <bloodcreed21@gmail.com>
2022-01-28 14:50:34 +0100

    almost finished, added options

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-const evoLog = (message) => {
+
+/**
+ * 
+ * @param {String} message 
+ * @param {{backgroundColor: String, bandColor: String, textColor: String, duration: number}} options 
+ */
+const evoLog = (message, options) => {
+
+  options = options || { backgroundColor: '#000', bandColor: '#ff0000', textColor: '#fff', duration: 5000 };
 
   //Default to console.log
   console.log(message);
 
-  //Print to the to the browser
-  showConsoleLogPopoup(message);
+  //Print the console.log also to the browser
+  showConsoleLogPopoup(message, options);
 };
 
-const containerStyle = "background-color: grey;padding: 1rem; border-radius: 6px; color: white; font-family: sans; width: max-content; float: right; position: relative";
 
-const bandStyle = "position:absolute; height: 100%; width:4px; background-color:red; left:0; top:0; border-radius: 6px 0 0 6px";
+/**
+ * 
+ * @param {String} message 
+ * @param {{backgroundColor: String, bandColor: String, textColor: String, duration: number}} options 
+ */
+const showConsoleLogPopoup = (message, options) => {
 
+  const containerStyle = `background-color: ${options.backgroundColor};padding: 1rem; border-radius: 6px; color: ${options.textColor}; font-family: sans; width: max-content; float: right; position: relative`;
+  const bandStyle = `position:absolute; height: 100%; width:4px; background-color:${options.bandColor}; left:0; top:0; border-radius: 6px 0 0 6px`;
 
-const showConsoleLogPopoup = (message) => {
   //Create a new div
   let newDiv = document.createElement('div');
   let bandDiv = document.createElement('div');
@@ -21,27 +34,19 @@ const showConsoleLogPopoup = (message) => {
   newDiv.style = containerStyle;
   newDiv.innerHTML = message;
 
-  //Add the band
+  //Add the band and stylize it
   bandDiv.style = bandStyle;
   newDiv.appendChild(bandDiv);
 
   //Append to the body
   document.body.appendChild(newDiv);
 
-  //Fade out
-  setTimeout(() => {
-    newDiv.classList.add('fade-out');
-  }, 1000);
-
   //Remove after 1 second
   setTimeout(() => {
     newDiv.remove();
-  }, 1000);
+  }, options.duration);
 };
 
-/*
-<div style="background-color: grey;padding: 1rem; border-radius: 6px; color: white; font-family: sans; width: max-content; float: right; position: relative">
-  <div style="position:absolute; height: 100%; width:4px; background-color:red; left:0; top:0; border-radius: 6px 0 0 6px"></div>
-  Prova
-</div>
-*/
+
+
+export default evoLog;
