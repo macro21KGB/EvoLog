dafef76351f4af7919b7fdb23d5d4a00e26e0fe0
Mario De Luca <bloodcreed21@gmail.com>
2022-03-30 18:48:12 +0200

    Reworked version with OOP

--- src/index.js
+++ src/index.js
@@ -1,71 +1,86 @@
-const defaultOptions = {
-  backgroundColor: '#2a2a2a',
-  bandColor: '#002064',
-  textColor: '#fff',
-  duration: 1000,
+
+
+
+
+class EvoLog {
+
+  message = "";
+  options = {};
+  parentDiv;
+
+  constructor() {
+
+    const defaultOptions = {
+      backgroundColor: '#2a2a2a',
+      bandColor: '#002064',
+      textColor: '#fff',
+      duration: 1000,
+    }
+
+    this.options = Object.assign({}, defaultOptions);
+
+    this.parentDiv = document.createElement('div');
+
+    const parentDivStyle = `
+      display: flex;
+      position: fixed;
+      top: 0;
+      right: 0;
+      flex-direction: column;
+      gap: 0.3rem;
+    `;
+
+    this.parentDiv.style = parentDivStyle;
+    document.body.appendChild(this.parentDiv);
+
+  }
+
+
+  /**
+   * 
+   * @param {String} message 
+   * @param {{backgroundColor: String, bandColor: String, textColor: String, duration: number}} options 
+   */
+  showConsoleLogPopoup = (message, options) => {
+    const containerStyle = `background-color: ${options.backgroundColor};padding: 1rem; margin:0.3rem; border-radius: 6px; color: ${options.textColor}; font-family: sans; max-width:40vw; position: relative;`;
+    const bandStyle = `position:absolute; height: 100%; width:4px; background-color:${options.bandColor}; left:0; top:0; border-radius: 6px 0 0 6px`;
+
+
+    //Create a new div
+    let newDiv = document.createElement('div');
+    let bandDiv = document.createElement('div');
+
+    //Set the style
+    newDiv.style = containerStyle;
+    newDiv.innerHTML = message;
+
+    //Add the band and stylize it
+    bandDiv.style = bandStyle;
+    newDiv.appendChild(bandDiv);
+
+    //Append to the body
+    this.parentDiv.appendChild(newDiv);
+
+    //Remove after 1 second
+    setTimeout(() => {
+      newDiv.remove();
+    }, options.duration);
+  };
+
+
+  log(message, _options) {
+    this.message = message;
+    let updatedOptions = {};
+    updatedOptions = Object.assign(this.options, _options);
+
+    console.log(this.message);
+    this.showConsoleLogPopoup(this.message, updatedOptions);
+
+    this.message = "";
+  }
 }
 
 
-/**
- * 
- * @param {String} message 
- * @param {{backgroundColor: String?, bandColor: String?, textColor: String?, duration: number}} options 
- */
-const evoLog = (message, options) => {
-
-  let updatedOptions = {};
-
-  if (options !== undefined) {
-    updatedOptions.backgroundColor = options.backgroundColor || defaultOptions.backgroundColor;
-    updatedOptions.bandColor = options.bandColor || defaultOptions.bandColor;
-    updatedOptions.textColor = options.textColor || defaultOptions.textColor;
-    updatedOptions.duration = options.duration || defaultOptions.duration;
-  }
-  else {
-    updatedOptions = defaultOptions;
-  }
-
-  //Default to console.log
-  console.log(message);
-
-  //Print the console.log also to the browser window
-  showConsoleLogPopoup(message, updatedOptions);
-};
 
 
-
-/**
- * 
- * @param {String} message 
- * @param {{backgroundColor: String, bandColor: String, textColor: String, duration: number}} options 
- */
-const showConsoleLogPopoup = (message, options) => {
-
-
-  const containerStyle = `background-color: ${options.backgroundColor};padding: 1rem; margin:0.3rem; border-radius: 6px; color: ${options.textColor}; font-family: sans; width: max-content; float: right; position: absolute; top:0; right:0;`;
-  const bandStyle = `position:absolute; height: 100%; width:4px; background-color:${options.bandColor}; left:0; top:0; border-radius: 6px 0 0 6px`;
-
-  //Create a new div
-  let newDiv = document.createElement('div');
-  let bandDiv = document.createElement('div');
-
-  //Set the style
-  newDiv.style = containerStyle;
-  newDiv.innerHTML = message;
-
-  //Add the band and stylize it
-  bandDiv.style = bandStyle;
-  newDiv.appendChild(bandDiv);
-
-  //Append to the body
-  document.body.appendChild(newDiv);
-
-  //Remove after 1 second
-  setTimeout(() => {
-    newDiv.remove();
-  }, options.duration);
-};
-
-
-
-export default evoLog;
+export default EvoLog;
